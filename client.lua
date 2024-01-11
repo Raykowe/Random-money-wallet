c99e9b8f40dd6a90fb59d67c376a8169e63020ad
Raykowe <125857039+Raykowe@users.noreply.github.com>
2024-01-11 19:31:59 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,2 @@
-RegisterNetEvent('random_cash:use', function()
-    -- This is where you can add client-side functionalities.
-    -- For example, playing an animation, sound, or showing a special effect.
-
-    -- Example: Trigger a simple notification (you can replace this with more complex functionality)
-    QBCore.Functions.Notify("You are opening the Mystery Cash Envelope...", "primary", 5000)
-end)
+QBCore.Functions.CreateUseableItem("random_cash", function(source)
+    print("Random Cash item used by: "..source)  -- This line is for debugging
